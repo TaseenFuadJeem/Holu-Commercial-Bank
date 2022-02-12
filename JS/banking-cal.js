cfc2f603b04a49b64c630f7deb81f667f923b1f1
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-02-12 13:58:27 +0600

    withdraw section of js completed

--- JS/banking-cal.js
+++ JS/banking-cal.js
@@ -13,3 +13,20 @@ function deposit() {
     // empty the deposit value
     depositValue.value = '';
 }
+
+function withdraw() {
+    // brought the input value of withdraw input section.
+    let withdrawValue = document.getElementById('withdraw-input');
+    // brought the text of total withdraw output section.
+    let totalWithdrawOutput = document.getElementById('Total-Withdraw');
+    // brought the text of total available balance
+    let availableBalance = document.getElementById('available-amount');
+
+    // total amount of withdraw
+    totalWithdrawOutput.innerText = parseFloat(totalWithdrawOutput.innerText) + parseFloat(withdrawValue.value);
+    // update the available balance
+    availableBalance.innerText = parseFloat(availableBalance.innerText) - parseFloat(withdrawValue.value);
+    // empty the withdraw value
+    withdrawValue.value = '';
+
+}
